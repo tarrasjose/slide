aeed8e378b8f7d8b91f8dcda7025b1a6d6efdaf8
tarrasjose <104996954+tarrasjose@users.noreply.github.com>
2023-04-17 11:33:30 -0300

    custom controls estilizados

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
 // console.log(slide);
-slide.addControl();
-console.log(slide);
+slide.addControl('.custom-controls');
+// console.log(slide);
 
 
 // console.log(slide);
